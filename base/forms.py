6ad2166983e6857dc5c230064b954b39f6ac936e
mubaarock <mustophamubarack17@gmail.com>
2024-05-08 09:56:18 +0100

    Add input field for tags in PostForm Create new ModelForm for Comment Model.

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,20 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
+    tags = forms.CharField(
+        label="Post Tags",
+        help_text="Type comma separated post tags",
+        required=False,
+    )
+
     class Meta:
         model = Post
         fields = ["title", "description", "image"]
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["description"]
