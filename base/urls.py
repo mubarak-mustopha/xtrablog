ca27622e26b6ebb2aea6fb137f00498ca3ae115a
mubaarock <mustophamubarack17@gmail.com>
2024-05-13 22:43:17 +0100

    Change pk type to uuid in url pattern

--- base/urls.py
+++ base/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.HomePageView.as_view(), name="home"),
     path("create-post/", views.CreatePostView.as_view(), name="create-post"),
-    path("<str:pk>/<slug:slug>/", views.PostView.as_view(), name="post"),
+    path("<uuid:pk>/<slug:slug>/", views.PostView.as_view(), name="post"),
     path(
-        "update/<str:pk>/<slug:slug>/",
+        "update/<uuid:pk>/<slug:slug>/",
         views.UpdatePostView.as_view(),
         name="update-post",
     ),
