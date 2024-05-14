23b3eb277e25a1ad318a7619d4726dea3393ef01
mubaarock <mustophamubarack17@gmail.com>
2024-05-14 11:01:16 +0100

    Avoid creating tag with empty label.

--- base/utils.py
+++ base/utils.py
@@ -3,12 +3,14 @@ from .models import Tag
 
 def add_post_tags(post, tag_labels):
     """
-    Add tags with labels from tag_labels 
+    Add tags with labels from tag_labels
     to post object.
     """
-    for tag in tag_labels:
-        tag = tag.strip().capitalize()
-        tag_obj, created = Tag.objects.get_or_create(name=tag)
+    for label in tag_labels:
+        label = label.strip().capitalize()
+        if not label:  # if label is an empty string.
+            continue
+        tag_obj, created = Tag.objects.get_or_create(name=label)
         if created:
             tag_obj.save()
         post.tags.add(tag_obj)
