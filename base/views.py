3dd87e5594a5ff18feed2a74d87e59a6c0ee34cb
mubaarock <mustophamubarack17@gmail.com>
2024-05-20 09:50:21 +0100

    View to handle post update.

--- base/views.py
+++ base/views.py
@@ -6,8 +6,8 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 from .models import Post, Tag
-from .forms import PostForm, CommentForm
-from .utils import add_post_tags
+from .forms import PostForm, CommentForm, PostUpdateForm
+from .utils import add_post_tags, remove_post_tags
 
 # Create your views here.
 
@@ -21,35 +21,62 @@ class HomePageView(View):
 @method_decorator(login_required, name="dispatch")
 class CreatePostView(View):
     form_class = PostForm
+    context = {"title": "Create"}
 
     def get(self, request):
         form = self.form_class()
-        return render(request, "post_form.html", {"form": form})
+        self.context.update({"form": form})
+        return render(request, "post_form.html", self.context)
 
     def post(self, request):
         form = self.form_class(request.POST, files=request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.user = request.user
-            tags = form.cleaned_data["tags"].split(",")
+            tags = form.cleaned_data["tags"]
             post.save()
             add_post_tags(post, tags)
             post.save()
             messages.success(request, "Post successfully created.")
             return redirect(post)
-        return render(request, "post_form.html", {"form": form})
+        self.context.update({"form": form})
+        return render(request, "post_form.html", self.context)
 
 
 @method_decorator(login_required, name="dispatch")
 class UpdatePostView(View):
-    form_class = PostForm
+    form_class = PostUpdateForm
+    context = {"title": "Update"}
 
     def get(self, request, pk, slug):
         post = Post.objects.get(id=pk, slug=slug)
-        form = self.form_class(instance=post)
         tag_names = post.tags.values_list("name", flat=True)
-        form.fields["tags"].initial = ", ".join(tag_names)
-        return render(request, "post_form.html", {"form": form})
+        initial_tags = ", ".join(tag_names)
+        form = self.form_class(
+            instance=post,
+            initial={
+                "tags": initial_tags,
+                "initial_tags": initial_tags,
+            },
+        )
+        self.context["form"] = form
+        return render(request, "post_form.html", self.context)
+
+    def post(self, request, pk, slug):
+        post = Post.objects.get(id=pk, slug=slug)
+        form = self.form_class(request.POST, files=request.FILES, instance=post)
+        if form.is_valid():
+            updated_tags = form.cleaned_data["tags"]
+            initial_tags = form.cleaned_data["initial_tags"]
+            removed_tags = set(initial_tags).difference(updated_tags)
+            added_tags = set(updated_tags).difference(initial_tags)
+            add_post_tags(post, added_tags)
+            remove_post_tags(post, removed_tags)
+            messages.success(request, "Post successfully updated.")
+            return redirect(post)
+        else:
+            self.context["form"] = form
+            return render(request, "post_form.html", self.context)
 
 
 @method_decorator(login_required, name="dispatch")
