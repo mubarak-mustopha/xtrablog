6852f4d28daf8ab290022a44d19af8431a2fd917
mubaarock <mustophamubarack17@gmail.com>
2024-05-20 09:46:27 +0100

    Add new function to remove a collection of tags from a post object.

--- base/utils.py
+++ base/utils.py
@@ -1,16 +1,23 @@
 from .models import Tag
 
 
-def add_post_tags(post, tag_labels):
+def add_post_tags(post, tag_names):
     """
     Add tags with labels from tag_labels
     to post object.
     """
-    for label in tag_labels:
-        label = label.strip().capitalize()
-        if not label:  # if label is an empty string.
-            continue
-        tag_obj, created = Tag.objects.get_or_create(name=label)
+    for name in tag_names:
+        tag_obj, created = Tag.objects.get_or_create(name=name)
         if created:
             tag_obj.save()
         post.tags.add(tag_obj)
+
+
+def remove_post_tags(post, tag_names):
+    """
+    Remove tags with labels from tag_labels
+    from post object.
+    """
+    for name in tag_names:
+        tag_obj = Tag.objects.get(name=name)
+        post.tags.remove(tag_obj)
